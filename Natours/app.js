40126f341fa4a4c7db7f6c4c7c8d1810d6f47f74
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2023-10-15 18:16:22 +0300

    Express

---
+++ Natours/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRouter');
+const userRouter = require('./routes/userRouter');
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json()).use(express.static(`${__dirname}/public`));
+
+app.use('/api/v1/tours', tourRouter).use('/api/v1/users', userRouter);
+
+module.exports = app;
