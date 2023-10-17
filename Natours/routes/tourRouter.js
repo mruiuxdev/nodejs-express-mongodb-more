ceddb714d3acb0c84f533eb29145449bafc93d12
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2023-10-17 20:44:10 +0300

    Aggregation matching - grouping

--- Natours/routes/tourRouter.js
+++ Natours/routes/tourRouter.js
@@ -6,12 +6,14 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
 // router.param('id', checkID);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+router.route('/tour-stats').get(getTourStats);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
